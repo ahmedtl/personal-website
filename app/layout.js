7eb1762303bc64cd6c2db7f281558ef962a70d51
AhmedTlili <61381757+ahmedtl@users.noreply.github.com>
2025-01-27 12:18:15 +0100

    umami settings

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="acea1353-b7ac-467a-bf61-ec6bdd05c288"></script>
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
